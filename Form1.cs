ebef99c8a4e65b71db1eaa78b611ecd4885294e5
Vĩ Bùi <vibuixuan2812@gmail.com>
2023-10-19 01:02:50 +0700

    Have idea about checking MSSV

--- Form1.cs
+++ Form1.cs
@@ -35,18 +35,43 @@ namespace Desktop_Homework2
             a.TSV = txtTenSV.Text;
             a.MSSV = txtMSSV.Text;
             a.DIEM = Double.Parse(txtDiem.Text);
-            A.DSSV.Add(a); 
+            /*for (int i = 0; i < A.DSSV.ToArray().Length; i++) //ý tưởng Check MSSV nếu trùng thì hiện thông báo
+            {
+                if (txtMSSV.Text == A.DSSV[i].MSSV)
+                {
+                    a.TSV = A.DSSV[i].TSV;
+                    a.DIEM = A.DSSV[i].DIEM;
+                    txtThongbao.Text = $"Sinh viên có MSSV là {A.DSSV[i].MSSV} vừa được cập nhật";
+                }
+            }*/
+            A.DSSV.Add(a);
+
         }
 
         private void btnOK_Click(object sender, EventArgs e)
         {
             Sinhvien TopSV = A.Top1();
             listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            /*if (txtThongbao.Text.Length != 0)
+                listBox2.Items.Add($"Sỉ số: {(A.Siso() - 1)}");
+            else listBox2.Items.Add($"Sỉ số: {A.Siso()}");*/
             listBox1.Items.Add($"Tên Sinh Viên: {a.TSV}");
             listBox1.Items.Add($"Mã Số Sinh Viên: {a.MSSV}");
             listBox1.Items.Add($"Điểm: {a.DIEM}");
-            listBox1.Items.Add($"Sỉ số: {A.Siso()}");
+            listBox2.Items.Add($"Sỉ số: {A.Siso()}");
+            listBox1.Items.Add("-----------------");
             listBox1.Items.Add($"Top 1 là: {TopSV.TSV} - Điểm là: {TopSV.DIEM}");
+        }
+
+        private void errorProvider1_RightToLeftChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMSSV_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
